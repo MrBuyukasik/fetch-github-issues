ca8a4b1059877d8c4757c9f2386d98dedeb8690a
Hasan Can <hasancanbuyukasik@gmail.com>
2022-04-29 02:02:00 +0300

    Remove useless history files

--- .history/src/pages/Home_20220426204356.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components/native';
-
-const Home = () => {
-
-}
-
-export default Home
--- .history/src/pages/Home_20220426204419.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components/native';
-
-const Home = () => {
-
-    return (<Text>Test</Text>)
-}
-
-export default Home
